7e9d4010e02d837edbf0895fd63f00cb04951abf
julienschuermans <julien.schuermans@ml6.eu>
2023-10-16 17:43:35 +0200

    Add example for CC

---
+++ examples/cc_2023Q3/containerview_webapp.py
@@ -0,0 +1,7 @@
+from workspace import workspace, webapp
+
+workspace.ContainerView(
+    webapp,
+    "Container view of Fantastic Web App",
+    "View on the different containers inside the Fantastic Web App."
+)
---
+++ examples/cc_2023Q3/systemlandscapeview.py
@@ -0,0 +1,6 @@
+from workspace import workspace
+
+workspace.SystemLandscapeView(
+    "Landscape View of My Fantastic Solution",
+    "The high level overview of My Fantastic Solution."
+)
